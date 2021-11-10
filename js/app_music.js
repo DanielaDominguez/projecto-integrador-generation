f00a913bc324ce193728e8b71fca008d430accdf
DanielaDominguez <90283692+DanielaDominguez@users.noreply.github.com>
2021-11-10 21:28:50 +0000

    Deploying to gh-pages from @ DanielaDominguez/projecto-integrador-generation@f3f6cbe50bbbd3820acf1822ed0129f0a46d79d0 🚀

---
+++ js/app_music.js
@@ -0,0 +1,220 @@
+/* ------------------ FUNCIONES PARA EL PERFIL DE FRIDA ------------------*/
+var $perfilPaaxFrida = document.querySelector("#perfil-paax-frida")
+var $playMusicFrida = document.querySelector("#audioFrida")
+var $coverImgFrida = document.querySelector("#coverFrida")
+
+$perfilPaaxFrida.addEventListener("mouseover", () => {
+    activarCoverFrida()
+    reproducirMusicaFrida()
+})
+
+$perfilPaaxFrida.addEventListener("mouseout", () => {
+    desactivarCoverFrida()
+    detenerMusicaFrida()
+})
+
+function reproducirMusicaFrida() {
+    $playMusicFrida.play()
+}
+
+function detenerMusicaFrida() {
+    $playMusicFrida.pause()
+}
+
+function activarCoverFrida() {
+    $coverImgFrida.style.opacity = 0.5
+}
+
+function desactivarCoverFrida() {
+    $coverImgFrida.style.opacity = 1
+}
+
+
+/* ------------------ FUNCIONES PARA EL PERFIL DE DANIELA ------------------*/
+var $perfilPaaxDani = document.querySelector("#perfil-paax-dani")
+var $playMusicDani = document.querySelector("#audioDani")
+var $coverImgDaniela = document.querySelector("#coverDaniela")
+
+$perfilPaaxDani.addEventListener("mouseover", () => {
+    activarCoverDaniela()
+    reproducirMusicaDani()
+})
+
+$perfilPaaxDani.addEventListener("mouseout", () => {
+    desactivarCoverDaniela()
+    detenerMusicaDani()
+})
+
+function reproducirMusicaDani() {
+    $playMusicDani.play()
+}
+
+function detenerMusicaDani() {
+    $playMusicDani.pause()
+}
+
+function activarCoverDaniela() {
+    $coverImgDaniela.style.opacity = 0.5
+}
+
+function desactivarCoverDaniela() {
+    $coverImgDaniela.style.opacity = 1
+}
+
+/* ------------------ FUNCIONES PARA EL PERFIL DE DANIEL ------------------*/
+var $perfilPaaxDaniel = document.querySelector("#perfil-paax-daniel")
+var $playMusicDaniel = document.querySelector("#audioDaniel")
+var $coverImgDaniel = document.querySelector("#coverDaniel")
+
+$perfilPaaxDaniel.addEventListener("mouseover", () => {
+    activarCoverDaniel()
+    reproducirMusicaDaniel()
+})
+
+$perfilPaaxDaniel.addEventListener("mouseout", () => {
+    desactivarCoverDaniel()
+    detenerMusicaDaniel()
+})
+
+function reproducirMusicaDaniel() {
+    $playMusicDaniel.play()
+}
+
+function detenerMusicaDaniel() {
+    $playMusicDaniel.pause()
+}
+
+function activarCoverDaniel() {
+    $coverImgDaniel.style.opacity = 0.5
+}
+
+function desactivarCoverDaniel() {
+    $coverImgDaniel.style.opacity = 1
+}
+
+/* ------------------ FUNCIONES PARA EL PERFIL DE RICARDO ------------------*/
+var $perfilPaaxRicardo = document.querySelector("#perfil-paax-ricardo")
+var $playMusicRicardo = document.querySelector("#audioRicardo")
+var $coverImgRicardo = document.querySelector("#coverRicardo")
+
+$perfilPaaxRicardo.addEventListener("mouseover", () => {
+    activarCoverRicardo()
+    reproducirMusicaRicardo()
+})
+
+$perfilPaaxRicardo.addEventListener("mouseout", () => {
+    desactivarCoverRicardo()
+    detenerMusicaRicardo()
+})
+
+function reproducirMusicaRicardo() {
+    $playMusicRicardo.play()
+}
+
+function detenerMusicaRicardo() {
+    $playMusicRicardo.pause()
+}
+
+function activarCoverRicardo() {
+    $coverImgRicardo.style.opacity = 0.5
+}
+
+function desactivarCoverRicardo() {
+    $coverImgRicardo.style.opacity = 1
+}
+
+
+/* ------------------ FUNCIONES PARA EL PERFIL DE GERARDO ------------------*/
+var $perfilPaaxGerardo = document.querySelector("#perfil-paax-gerardo")
+var $playMusicGerardo = document.querySelector("#audioGerardo")
+var $coverImgGerardo = document.querySelector("#coverGerardo")
+
+$perfilPaaxGerardo.addEventListener("mouseover", () => {
+    activarCoverGerardo()
+    reproducirMusicaGerardo()
+})
+
+$perfilPaaxGerardo.addEventListener("mouseout", () => {
+    desactivarCoverGerardo()
+    detenerMusicaGerardo()
+})
+
+function reproducirMusicaGerardo() {
+    $playMusicGerardo.play()
+}
+
+function detenerMusicaGerardo() {
+    $playMusicGerardo.pause()
+}
+
+function activarCoverGerardo() {
+    $coverImgGerardo.style.opacity = 0.5
+}
+
+function desactivarCoverGerardo() {
+    $coverImgGerardo.style.opacity = 1
+}
+
+
+/* ------------------ FUNCIONES PARA EL PERFIL DE VÍCTOR ------------------*/
+var $perfilPaaxVictor = document.querySelector("#perfil-paax-victor")
+var $playMusicVictor = document.querySelector("#audioVictor")
+var $coverImgVictor = document.querySelector("#coverVictor")
+
+$perfilPaaxVictor.addEventListener("mouseover", () => {
+    activarCoverVictor()
+    reproducirMusicaVictor()
+})
+
+$perfilPaaxVictor.addEventListener("mouseout", () => {
+    desactivarCoverVictor()
+    detenerMusicaVictor()
+})
+
+function reproducirMusicaVictor() {
+    $playMusicVictor.play()
+}
+
+function detenerMusicaVictor() {
+    $playMusicVictor.pause()
+}
+
+function activarCoverVictor() {
+    $coverImgVictor.style.opacity = 0.5
+}
+
+function desactivarCoverVictor() {
+    $coverImgVictor.style.opacity = 1
+}
+
+
+/* ------------------ FUNCIONES PARA EL PERFIL DE NIVER ------------------*/
+var $perfilPaaxNiver = document.querySelector("#perfil-paax-niver")
+var $playMusicNiver = document.querySelector("#audioNiver")
+var $coverImgNiver = document.querySelector("#coverNiver")
+
+$perfilPaaxNiver.addEventListener("mouseover", () => {
+    activarCoverNiver()
+    reproducirMusicaNiver()
+})
+
+$perfilPaaxNiver.addEventListener("mouseout", () => {
+    desactivarCoverNiver()
+    detenerMusicaNiver()
+})
+
+function reproducirMusicaNiver() {
+    $playMusicNiver.play()
+}
+
+function detenerMusicaNiver() {
+    $playMusicNiver.pause()
+}
+
+function activarCoverNiver() {
+    $coverImgNiver.style.opacity = 0.5
+}
+
+function desactivarCoverNiver() {
+    $coverImgNiver.style.opacity = 1
+}
